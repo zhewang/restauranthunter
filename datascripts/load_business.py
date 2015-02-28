2e341e4e64ab4cf89c5e00b3bc490ac7c8d73027
Zhe Wang <zwang.contact@gmail.com>
2015-02-27 23:02:34 -0700

    insert business data to table

--- datascripts/load_business.py
+++ datascripts/load_business.py
@@ -1,17 +1,26 @@
 import json
+import pymysql
 
-def InsertToMySQL(record):
-    print(record['name'])
+def InsertToMySQL(conn, record):
+    cursor = conn.cursor()
+    query = "INSERT business values ('{0}',\"{1}\",\"{2}\",'{3}','{4}',{5},{6},{7},{8},{9})".format(
+        record['business_id'],record['name'],record['full_address'].replace('\n',', '),record['city'],record['state'],
+        record['latitude'], record['longitude'],record['stars'],record['review_count'],record['open'])
+    cursor.execute( query )
 
 def main(fileName):
+    conn = pymysql.Connect(host='localhost', user='root', passwd='', db='yelpdb')
+
     with open(fileName,'r') as f:
         for line in f:
             record = json.loads(line)
-            InsertToMySQL(record)
+            InsertToMySQL(conn, record)
+
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
 
     fileName = "test_business.json"
     main(fileName)
-
-
+    
