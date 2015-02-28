4343d1bad1286310b6f706c8f9a3da841f919934
Zhe Wang <zwang.contact@gmail.com>
2015-02-28 15:41:49 -0700

    fix typo: data->date

--- datascripts/create_review_table.sql
+++ datascripts/create_review_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE review (
     business_id CHAR(22) NOT NULL,
     user_id CHAR(22) NOT NULL,
     stars FLOAT(2,1),
-    data CHAR(10),
+    date CHAR(10),
     PRIMARY KEY (review_id),
     FOREIGN KEY (business_id) REFERENCES business (business_id),
     FOREIGN KEY (user_id) REFERENCES user (user_id)
