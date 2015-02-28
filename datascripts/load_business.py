179786a33ad11a1a890f18b13d150d0feefb4c35
Zhe Wang <zwang.contact@gmail.com>
2015-02-28 13:53:55 -0700

    only insert restaurants & add parser

--- datascripts/load_business.py
+++ datascripts/load_business.py
@@ -1,10 +1,11 @@
 import json
 import pymysql
+import argparse
 
 def InsertToMySQL(conn, record):
     cursor = conn.cursor()
     query = "INSERT business values ('{0}',\"{1}\",\"{2}\",'{3}','{4}',{5},{6},{7},{8},{9})".format(
-        record['business_id'],record['name'],record['full_address'].replace('\n',', '),record['city'],record['state'],
+        record['business_id'],record['name'],record['full_address'].replace('\n',', ').encode('utf-8'),record['city'],record['state'],
         record['latitude'], record['longitude'],record['stars'],record['review_count'],record['open'])
     cursor.execute( query )
 
@@ -14,13 +15,17 @@ def main(fileName):
     with open(fileName,'r') as f:
         for line in f:
             record = json.loads(line)
-            InsertToMySQL(conn, record)
+            if 'Restaurants' in record['categories']:
+                InsertToMySQL(conn, record)
 
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
 
-    fileName = "test_business.json"
-    main(fileName)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fileName", help="Json file path")
+    args = parser.parse_args()
+
+    main(args.fileName)
     
