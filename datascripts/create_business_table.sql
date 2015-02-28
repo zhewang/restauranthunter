95deb930a2dce0752881a1caa73190e701b17bd2
Zhe Wang <zwang.contact@gmail.com>
2015-02-27 23:01:59 -0700

    finish sql to create business table

--- datascripts/create_business_table.sql
+++ datascripts/create_business_table.sql
@@ -1,5 +1,13 @@
 CREATE TABLE business (
     business_id CHAR(22),
-    name TEXT,
-    PRIMARY KEY (businessID)
+    name VARCHAR(255) ,
+    full_address VARCHAR(255),
+    city VARCHAR(30),
+    state VARCHAR(5),
+    latitude DOUBLE(10,6),
+    longitude DOUBLE(10,6),
+    stars FLOAT(2),
+    review_count INT,
+    open BOOLEAN,
+    PRIMARY KEY (business_id)
 );
