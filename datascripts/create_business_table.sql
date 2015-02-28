2b85cd3fde736a2d47d1357a47a71af8cbc2bcee
Zhe Wang <zwang.contact@gmail.com>
2015-02-28 14:56:22 -0700

    change city from varchar(30) to varchar(127)

--- datascripts/create_business_table.sql
+++ datascripts/create_business_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE business (
     business_id CHAR(22),
     name VARCHAR(255) ,
     full_address VARCHAR(255),
-    city VARCHAR(30),
+    city VARCHAR(127),
     state VARCHAR(5),
     latitude DOUBLE(10,6),
     longitude DOUBLE(10,6),
