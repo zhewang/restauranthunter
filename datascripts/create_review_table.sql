e738711c7f120df3c8364fde90204932837904ce
Zhe Wang <zwang.contact@gmail.com>
2015-02-28 16:06:30 -0700

    use index instead of foreign key to avoid insert error

--- datascripts/create_review_table.sql
+++ datascripts/create_review_table.sql
@@ -5,6 +5,6 @@ CREATE TABLE review (
     stars FLOAT(2,1),
     date CHAR(10),
     PRIMARY KEY (review_id),
-    FOREIGN KEY (business_id) REFERENCES business (business_id),
-    FOREIGN KEY (user_id) REFERENCES user (user_id)
+    INDEX (business_id),
+    INDEX (user_id)
 );
