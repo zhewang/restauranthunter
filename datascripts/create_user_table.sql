5d75ea4c0fcc2f446e1df577b46db5e88f97e387
Zhe Wang <zwang.contact@gmail.com>
2015-02-28 15:07:47 -0700

    fix typo: starts->stars

--- datascripts/create_user_table.sql
+++ datascripts/create_user_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE user (
     user_id CHAR(22),
     name VARCHAR(255),
     review_count INT,
-    average_starts FLOAT(3,2),
+    average_stars FLOAT(3,2),
     yelping_since CHAR(7),
     PRIMARY KEY (user_id)
 );
