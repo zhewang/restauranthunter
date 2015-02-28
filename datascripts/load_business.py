dc7fe92cb6665a688ce09b9273a445981ad7b150
Zhe Wang <zwang.contact@gmail.com>
2015-02-28 14:57:15 -0700

    fix utf-8 encoding problem

--- datascripts/load_business.py
+++ datascripts/load_business.py
@@ -4,13 +4,13 @@ import argparse
 
 def InsertToMySQL(conn, record):
     cursor = conn.cursor()
-    query = "INSERT business values ('{0}',\"{1}\",\"{2}\",'{3}','{4}',{5},{6},{7},{8},{9})".format(
-        record['business_id'],record['name'],record['full_address'].replace('\n',', ').encode('utf-8'),record['city'],record['state'],
-        record['latitude'], record['longitude'],record['stars'],record['review_count'],record['open'])
-    cursor.execute( query )
 
+    cursor.execute(  "INSERT business values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        [record['business_id'],record['name'].encode('utf-8'),record['full_address'].encode('utf-8'),record['city'].encode('utf-8'),record['state'],
+        record['latitude'], record['longitude'],record['stars'],record['review_count'],record['open']] )
+   
 def main(fileName):
-    conn = pymysql.Connect(host='localhost', user='root', passwd='', db='yelpdb')
+    conn = pymysql.Connect(host='localhost', user='root', passwd='',charset='utf8', db='yelpdb')
 
     with open(fileName,'r') as f:
         for line in f:
