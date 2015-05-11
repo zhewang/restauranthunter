8f30c7a0af61fb6d7164c38def6bde6ec24cbee4
Zhe Wang <zwang.contact@gmail.com>
2015-05-10 20:56:08 -0700

    Merge with laiyong's code

--- map.js
+++ map.js
@@ -3,50 +3,62 @@ var restaurant_data; //[business_id, name, lon, lat, average_rating]
 // custom marker for selected restaurant
 var plainMarker = L.icon({
     iconUrl: './leaflet-0.8-dev/images/marker-icon.png',
+    shadowUrl: './leaflet-0.8-dev/images/marker-shadow.png',
 
     iconSize:    [25, 41],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
+    shadowSize:  [41, 41]
 });
 
 var starMarker1 = L.icon({
     iconUrl: './leaflet-0.8-dev/images/marker-star1.png',
+    shadowUrl: './leaflet-0.8-dev/images/marker-shadow.png',
 
     iconSize:    [25, 41],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
+    shadowSize:  [41, 41]
 });
 
 var starMarker2 = L.icon({
     iconUrl: './leaflet-0.8-dev/images/marker-star2.png',
+    shadowUrl: './leaflet-0.8-dev/images/marker-shadow.png',
 
     iconSize:    [25, 41],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
+    shadowSize:  [41, 41]
 });
 
 var starMarker3 = L.icon({
     iconUrl: './leaflet-0.8-dev/images/marker-star3.png',
+    shadowUrl: './leaflet-0.8-dev/images/marker-shadow.png',
 
     iconSize:    [25, 41],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
+    shadowSize:  [41, 41]
 });
 
 var starMarker4 = L.icon({
     iconUrl: './leaflet-0.8-dev/images/marker-star4.png',
+    shadowUrl: './leaflet-0.8-dev/images/marker-shadow.png',
 
     iconSize:    [25, 41],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
+    shadowSize:  [41, 41]
 });
 
 var starMarker5 = L.icon({
     iconUrl: './leaflet-0.8-dev/images/marker-star5.png',
+    shadowUrl: './leaflet-0.8-dev/images/marker-shadow.png',
 
     iconSize:    [25, 41],
     iconAnchor:  [12, 41],
     popupAnchor: [1, -34],
+    shadowSize:  [41, 41]
 });
 
 function GetMarkerbyStar(star) {
@@ -70,6 +82,22 @@ var SelectedMarkerZIndex = [];
 var selectedID = [];
 var BrushedResID = []
 
+var yScale = d3.scale.linear().domain([0, 5]).range([150, 50]);
+var mindate = new Date("Janurary 1, 2011 00:00:00");
+var maxdate = new Date("Janurary 1, 2015 00:00:00");
+var yearFormat = d3.time.format("%Y");
+var xScale = d3.time.scale().domain([mindate, maxdate]).range([50, 750]);
+var allIDs;   // restaurant ID for all the restaurants
+var rating;     // store the the json file after importing
+
+// funtiont generate date object from database entry
+var formatDate = d3.time.format("%Y-%m-%d");
+
+
+d3.json("reviews.json", function(json) {
+        rating = json;
+    });
+
 d3.json("./az100.json", function(error, json) {
     if (error) return console.warn(error);
     restaurant_data = json;
@@ -85,12 +113,49 @@ d3.json("./az100.json", function(error, json) {
         styles: 'grayscale'
     }).addTo(map);
 
-
-    // draw the scatter plot (first rating vs average rating) axises
-    xaxisRange = d3.scale.linear().domain([0, 5]).range([40,390]);
+    // draw the scatter plot (weighted rating vs average rating) axises
+    xaxisRange = d3.scale.linear().domain([0, 5]).range([140,490]);
     yaxisRange = d3.scale.linear().domain([0, 5]).range([370,20]);
     var xAxis = d3.svg.axis().ticks(5).scale(xaxisRange);
     var yAxis = d3.svg.axis().ticks(5).scale(yaxisRange);
+
+    // color scale, Cynthia Brewer’s RdYlBu scale
+    var color01 = "#91bfdb";     //blue
+    var color02 = "#ffffbf";     //white
+    var color03 = "#fc8d59";     //red
+    var colorScale = d3.scale.linear().domain([0, 2.5, 5])
+        .range([color01, color02, color03]);
+
+    //axis of color scale map
+    var rateScale = d3.scale.linear().domain([5, 2.5, 0]).range([100, 200, 300]);
+    var rateAxis = d3.svg.axis().ticks(5).scale(rateScale);
+    rateAxis.orient("left");
+    var rateAxisSel = d3.select("#scatter") // or something else that selects the SVG element in your visualizations
+        .append("g") // create a group node
+        .attr("class", "axises")
+        .attr("transform", "translate(30, 0)")
+        .call(rateAxis); // call the axis generator
+
+    //plot the rating color scale
+    gradientPlot = function (c1, c2, c3)
+    {
+        var gradientScale = d3.scale.linear()
+        .domain([0, 50, 100])
+        .range([c3, c2, c1]);
+
+        for (k=0; k<100; k++)
+        {
+            d3.select("#scatter")
+            .append("rect")
+            .attr("width", 20)
+            .attr("height", 2)
+            .attr("x", 30)
+            .attr("y", 100 + k*2)
+            .style("fill", gradientScale(k));
+        }
+    }
+
+    gradientPlot(color01, color02, color03);
 
     // Add brush
     var brush = d3.select("#scatter").append("g")
@@ -119,8 +184,8 @@ d3.json("./az100.json", function(error, json) {
         BrushedResID = [];
 
         var extent = d3.event.target.extent();
-        d3.selectAll("circle").classed("hidden", function(d, i) {
-            inBrush = (extent[0][0] <= firstRat[i] && firstRat[i] <= extent[1][0]
+        d3.select("#scatter").selectAll("circle").classed("hidden", function(d, i) {
+            inBrush = (extent[0][0] <= weightedR[i] && weightedR[i] <= extent[1][0]
                       && extent[0][1] <= d && d <= extent[1][1]);
             if(inBrush) { BrushedResID.push(resID[i]);}
             return !inBrush;
@@ -131,7 +196,7 @@ d3.json("./az100.json", function(error, json) {
 
     function brushend() {
         if (d3.event.target.empty()) {
-            d3.selectAll("circle").classed("hidden", false)
+            d3.select("#scatter").selectAll("circle").classed("hidden", false)
             for(var i = 0; i < SelectedMarkers.length; i ++) {
                 SelectedMarkers[i].setOpacity(1);
                 SelectedMarkers[i].setZIndexOffset(SelectedMarkerZIndex[i]);
@@ -139,23 +204,21 @@ d3.json("./az100.json", function(error, json) {
         }
     }
 
-
+    // the equal line in the scatter plot
     d3.select("#scatter").append("line")
-        .attr("x1", 40)
+        .attr("x1", 140)
         .attr("y1", 370)
-        .attr("x2", 390)
+        .attr("x2", 490)
         .attr("y2", 20)
         .attr("stroke", "blue")
         .attr("stroke-width", 1);
 
+    // axis of the scatter plot
     yAxis.orient("left");
-    d3.select("#scatter") // or something else that selects the SVG element in your visualizations
-        .append("g") // create a group node
-        .attr("class", "axises")
-        .attr("transform", "translate(40, 0)")
+    d3.select("#scatter").append("g").attr("class", "axises")
+        .attr("transform", "translate(140, 0)")
         .call(yAxis); // call the axis generator
-     d3.select("#scatter") // or something else that selects the SVG element in your visualizations
-        .append("g") // create a group node
+     d3.select("#scatter").append("g")
         .attr("class", "axises")
         .attr("transform", "translate(0, 370)")
         .call(xAxis); // call the axis generator
@@ -165,13 +228,13 @@ d3.json("./az100.json", function(error, json) {
     .on("click", function()
     {
         var mouseC = d3.mouse(this);
-        for (var k=0; k<firstRat.length; k++){
-            if ((mouseC[0] + 5) > xaxisRange(firstRat[k]) && (mouseC[0] - 5) < xaxisRange(firstRat[k]))
+        for (var k=0; k<weightedR.length; k++){
+            if ((mouseC[0] + 5) > xaxisRange(weightedR[k]) && (mouseC[0] - 5) < xaxisRange(weightedR[k]))
             {
                 if ((mouseC[1] + 5) > yaxisRange(ratMean[k]) && (mouseC[1] - 5) < yaxisRange(ratMean[k]))
                 {
                     d3.select("body").select("#name").text(resName[k]);
-                    d3.select("body").select("#weiRating").text(firstRat[k]);
+                    d3.select("body").select("#weiRating").text(weightedR[k]);
                     d3.select("body").select("#aveRating").text(ratMean[k]);
                     d3.select("body").select("#numRating").text(numR[k]);
                     d3.select("body").select("#firRating").text(firstDate[k]);
@@ -180,18 +243,92 @@ d3.json("./az100.json", function(error, json) {
                     onCircle = true
                     highlightInMap([resID[k]]);
 
-                    // TODO update the bar chart, can't use plotbyID because this also update the scatterplot
-                    // We can use something like: UpdateReviewsChart(resIDs)
-                    // This function takes in a list of restaurant IDs and show the correspoinding reviews chart
-                    // without affecting the scatter plot and the underlying datas for the selected restaurants like
-                    // resName, firstRat, numR, ...
+                    // plot single restaurant rating through the time
+                    var revi1;
+                    restaurantID1 = selectedID[k];
+                    //console.log(restaurantID1);
+
+                    function importRating1 (rid)     // import the rating info of a selected restaurant
+                    {
+                        //console.log(rid);
+                        var numRatings = rating[rid].length; // number of ratings for this restaurant
+                        revi1 = new Array(numRatings);       // import ratings
+                        revit = new Array(numRatings);       // import ratings date information
+
+                            for (var h=0; h<numRatings; h++)
+                            {
+                                revi1[h] = rating[rid][h][0];
+                                revit[h] = formatDate.parse(rating[rid][h][1]);
+
+                            }
+                    }
+
+                    importRating1 (restaurantID1);
+
+                    // get the name of the restaurant
+                    function getNamebyID(id) {
+                        for (var i = 0; i < restaurant_data.length; i ++)
+                            if (restaurant_data[i][0] == id) {
+                                return restaurant_data[i][1];
+                            };
+                    }
+
+                    //var rName = getNamebyID(restaurantID1);
+
+                    d3.select("body").select("#historyPlot").selectAll("circle").remove();
+                    d3.select("body").select("#historyPlot").select("#rNm").remove();
+                    d3.select("#historyPlot").append("text")
+                        .attr("id", "rNm")
+                        .attr("x", 50)
+                        .attr("y", 20)
+                        .style("font-size", "16px")
+                        .text("---" + resName[k] + "---");
+
+                    d3.select("#historyPlot")
+                        .selectAll("circle")
+                        .data(revi1)
+                        .enter()
+                        .append("circle")
+                        .attr("cx", function(d,i) { return xScale(revit[i]); })
+                        .attr("cy", function(d) { return yScale(d); })
+                        .attr("r", function(d,i) { if (yearFormat(revit[i]) > 2010 && yearFormat(revit[i]) < 2015) return 5; else return 0;})
+                        .attr("fill", function(d,i) {if (i<firstN) return "red"; else return "blue";})
+                        .attr("stroke", "white")
+                        .attr("stroke-width", 0);
+
                 }
             }
         }
 
     });
 
+    // draw the initial history plot
+    d3.select("#historyPlot").append("text")
+        .attr("id", "rNm")
+        .attr("x", 50)
+        .attr("y", 20)
+        .style("font-size", "16px")
+        .text("------");
 
+    var yAxis = d3.svg.axis()
+        .orient("left")
+        .ticks(5)
+        .scale(yScale);
+
+    var xAxis = d3.svg.axis()
+        .orient("bottom")
+        .ticks(6)
+        .scale(xScale);
+
+    d3.select("#historyPlot").append("g")
+        .attr("class", "axises")
+        .attr("transform", "translate(50,0)")
+        .call(yAxis);
+
+    d3.select("#historyPlot").append("g")
+        .attr("class", "axises")  // give it a class so it can be used to select only xaxis labels  below
+        .attr("transform", "translate(0,150)")
+        .call(xAxis);
 
     // Add markers to map
     for (var i = 0; i < restaurant_data.length; i ++) {
@@ -232,15 +369,8 @@ d3.json("./az100.json", function(error, json) {
                 markers[i].setOpacity(1);
             }
 
-            // clean all the plots and table entries
-            d3.select("body").selectAll(".svgclass").remove();
-            d3.select("#scatter").selectAll("circle").remove();
-            d3.select("body").select("#name").text("");
-            d3.select("body").select("#weiRating").text("");
-            d3.select("body").select("#aveRating").text("");
-            d3.select("body").select("#numRating").text("");
-            d3.select("body").select("#firRating").text("");
-            d3.select("body").select("#lasRating").text("");
+            plotByID(allIDs);
+
         });
 
     // Highlight the marker when click on corresponding scatterplot
@@ -276,5 +406,14 @@ d3.json("./az100.json", function(error, json) {
 
         plotByID([this._leaflet_id]);
     }
+
+    // plot all the restaurant at the begining;
+    allIDs = new Array(restaurant_data.length);
+    for (var j=0; j < restaurant_data.length; j++)
+    {
+        allIDs[j] = restaurant_data[j][0];
+    }
+
+    plotByID(allIDs);
 
 });
--- yelp-resReview3.js
+++ yelp-resReview3.js
@@ -1,5 +1,6 @@
-var rating;     // store the the json file after importing
+
 var revi;       // store ratings for one restaruant
+var reviMax = new Array(2);    // store ratings for the restaruant with hightest ratings
 var revit;      // store the date of corresponding rating
 var firstN = 1;   //first firstN reviews will be compared with the total average
 var ratMean;    // store avearge rating of selected restaurant
@@ -10,12 +11,19 @@ var resID;    // store the restaurant ID of selected restaurant
 var numR;      // store how many ratings of each restaurant
 var firstDate;   // store the date of first rating of each restaurant
 var lastDate;  // store the date of last rating of each restuarant
+var weightedR;   // store the weighted ratings of selected restaurant
+//var rating;     // store the the json file after importing
 
 // funtiont generate date object from database entry
 var formatDate = d3.time.format("%Y-%m-%d");
 
+// d3.json("reviews.json", function(json) {
+//         rating = json;
+//     });
+
 function plotByID (business_id)
 {
+    
     // create new array to store mean rating for all the restaurant
     ratMean = new Array(business_id.length);
     firstRat = new Array(business_id.length);
@@ -24,25 +32,22 @@ function plotByID (business_id)
     firstDate = new Array(business_id.length);
     lastDate = new Array(business_id.length);
     resID = new Array(business_id.length);
+    weightedR = new Array(business_id.length);
     counter = 0;
 
-    d3.select("body").selectAll(".svgclass").remove();  // clean all the drawings
+    //console.log(business_id[0]);
 
     for (var j=0; j<business_id.length; j++)
     {
-        if (rating.hasOwnProperty(business_id[j])) {
-            svgname = "svg" + j.toString();
-
-            // create a svg for the new restaurant
-            d3.select("body").append("svg").attr("width", 800).attr("height", 200)
-                .attr("id", svgname).attr("class", "svgclass");
-
-            // ToDo use a key value array for restaurant data, so that
-            //      we can get name like restaurant_data["id"]
+        console.log(business_id[j]);
+        if (rating.hasOwnProperty(business_id[j])) 
+        //if (1 == 1)
+        {
             function getNamebyID(id) {
                 for (var i = 0; i < restaurant_data.length; i ++)
                     if (restaurant_data[i][0] == business_id[j]) {
-                        return restaurant_data[i][1]
+                        
+                        return restaurant_data[i][1];
                     };
             }
 
@@ -50,26 +55,25 @@ function plotByID (business_id)
             resID[j] = business_id[j]
             resName[j] = rName;
             restaurantID = business_id[j];            // restaurant id in the database
-            importRating(restaurantID);
-
-            plotRating(revi, revit, svgname, rName);
-
-            // initialize the table
-            d3.select("body").select("#name").text(resName[0]);
-            d3.select("body").select("#weiRating").text(firstRat[0]);
-            d3.select("body").select("#aveRating").text(ratMean[0]);
-            d3.select("body").select("#numRating").text(numR[0]);
-            d3.select("body").select("#firRating").text(firstDate[0]);
-            d3.select("body").select("#lasRating").text(lastDate[0]);
-
+            importRating(restaurantID);               // import data of one restaurant
+            
         } else {
             alert("Don't have reviews for this restaurant")
         }
     }
 
-    // draw first review (y) vs avearage review (x)
-    xaxisRange = d3.scale.linear().domain([0, 5]).range([40,390]);
+    maxWeight(weightedR, business_id);
+
+    // draw weighted rating (x) vs avearage rating (y)
+    xaxisRange = d3.scale.linear().domain([0, 5]).range([140,490]);
     yaxisRange = d3.scale.linear().domain([0, 5]).range([370,20]);
+
+    var color01 = "#91bfdb";     //blue
+    var color02 = "#ffffbf";     //white
+    var color03 = "#fc8d59";     //red
+    var colorScale = d3.scale.linear()
+        .domain([0, 2.5, 5])
+        .range([color01, color02, color03]);
 
     d3.select("#scatter").selectAll("circle").remove();
     d3.select("#scatter")//.append("svg").attr("width", 400).attr("height", 400).attr("id", "scatter")
@@ -77,115 +81,105 @@ function plotByID (business_id)
         .data(ratMean)
         .enter()
         .append("circle")
-        .attr("cx", function (d, i) { return xaxisRange(firstRat[i]);} )
+        .attr("cx", function (d, i) { return xaxisRange(weightedR[i]);} )
         .attr("cy", function (d) { return yaxisRange(d);})
-        .attr("r", 5)
-
+        .attr("fill", function (d) {return colorScale(d);})
+        .attr("r", 5);
+        
     // hide other circles when click on one
     d3.selectAll("circle")
         .on("click", function(d, i) {
-            d3.selectAll("circle").classed("hidden", function (e, j) {
+            d3.select("#scatter").selectAll("circle").classed("hidden", function (e, j) {
                 return d != e || i != j;
             });
         });
+}
+
+
+// calculate the weighted ratings for one restaurant
+function calWeight (reviSum, indexR, numOfRatings)
+{
+    //for (var k = 0; k<numOfRatings; k++)
+    var newMean = reviSum/(numOfRatings - 1.6);
+    weightedR[indexR] = (numOfRatings/(numOfRatings + 30)*newMean + 30/(numOfRatings + 30)*3).toFixed(1);
+}
+
+// calculate the restaurant with maximum weighted rating 
+function maxWeight (wR, business_id)
+{
+    var maxIndex = 0;
+    for (var k=0; k<wR.length-1; k++)
+    {
+        if (wR[maxIndex] < wR[k+1])
+            maxIndex = k+1;
+    }
+
+    console.log(counter, wR.length, weightedR[maxIndex]);
+    // update table
+    d3.select("body").select("#name").text(resName[maxIndex]);
+    d3.select("body").select("#weiRating").text(weightedR[maxIndex]);
+    d3.select("body").select("#aveRating").text(ratMean[maxIndex]);
+    d3.select("body").select("#numRating").text(numR[maxIndex]);
+    d3.select("body").select("#firRating").text(firstDate[maxIndex]);
+    d3.select("body").select("#lasRating").text(lastDate[maxIndex]);
+
+    // update hitory plot of one restaurant
+    d3.select("body").select("#historyPlot").selectAll("circle").remove();
+    d3.select("body").select("#historyPlot").select("#rNm").remove();
+    d3.select("#historyPlot").append("text")
+        .attr("id", "rNm").attr("x", 50).attr("y", 20).style("font-size", "16px")
+        .text("---" + resName[maxIndex] + "---");
+
+    maxResID = business_id[maxIndex];            // restaurant id in the database with max weighted rating
+    
+    // import the data of the restaurant with hightest weighted score
+    var numRatings = rating[maxResID].length; // number of ratings for this restaurant
+    reviMax[0] = new Array(numRatings);     // import ratings
+    reviMax[1] = new Array(numRatings);     // import date of ratings
+    for (var k=0; k<numRatings; k++)
+        {
+            reviMax[0][k] = rating[maxResID][k][0];
+            reviMax[1][k] = formatDate.parse(rating[maxResID][k][1]);
+        }
+
+    d3.select("#historyPlot")
+        .selectAll("circle")
+        .data(reviMax[0])
+        .enter()
+        .append("circle")
+        .attr("cx", function(d,i) { return xScale(reviMax[1][i]); })
+        .attr("cy", function(d) { return yScale(d); })
+        .attr("r", function(d,i) { if (yearFormat(reviMax[1][i]) > 2010 && yearFormat(reviMax[1][i]) < 2015) return 5; else return 0;})
+        .attr("fill", "red")
+        .attr("stroke", "white")
+        .attr("stroke-width", 0);
+
 }
 
 // function to load ratings for one restaurant
 function importRating (rid)
 {
     var numRatings = rating[rid].length; // number of ratings for this restaurant
+    var reviSum = 0;
+    
     revi = new Array(numRatings);         // import ratings
     revit = new Array(numRatings);        // import date of ratings
         for (var k=0; k<numRatings; k++)
         {
             revi[k] = rating[rid][k][0];
             revit[k] = formatDate.parse(rating[rid][k][1]);
-            //console.log(revi[k], revit[k]);
+            if (k == 0 || k == 1)
+                reviSum = reviSum + 0.2 * rating[rid][k][0];
+            else
+                reviSum = reviSum + rating[rid][k][0];
         }
-
+    
     firstDate[counter] = rating[rid][0][1];
     lastDate[counter] = rating[rid][numRatings-1][1];
     numR[counter] = numRatings;
     ratMean[counter] = (d3.mean(revi)).toFixed(1);
     firstRat[counter] = revi[0];
-    //console.log(ratMean[0]);
+    calWeight(reviSum, counter, numRatings);
     counter++;
-
 }
-
-var mindate = new Date("Janurary 1, 2011 00:00:00");
-var maxdate = new Date("Janurary 1, 2015 00:00:00");
-
-// plot the ratings
-function plotRating (revi_temp, revit_temp, svgname, rName)
-{
-
-    svgname = "#" + svgname;
-    var yScale = d3.scale.linear().domain([0, 5])    // ratings between 0 and 100
-        .range([150, 50]);
-
-    // var mindate = revit_temp[0];
-    // var maxdate = revit_temp[(revit_temp.length - 1)];
-
-    var xScale = d3.time.scale().domain([mindate, maxdate])
-        .range([50, 750]);
-
-    d3.select(svgname).selectAll("text").remove();
-
-    d3.select(svgname).append("text")
-        .attr("x", 50)
-        .attr("y", 20)
-        .style("font-size", "16px")
-        .text("---" + rName + "---");
-
-    var yearFormat = d3.time.format("%Y");
-    //console.log(yearFormat(revit_temp[0]));
-    d3.select(svgname)
-        .selectAll("rect")
-        .data(revi_temp)
-        .enter()
-        .append("rect")
-        .attr("width", 5)
-        .attr("height", function(d,i) { if (yearFormat(revit_temp[i]) > 2010 && yearFormat(revit_temp[i]) < 2015) return d*20; else return 0;})
-        .attr("x", function(d,i) { return xScale(revit_temp[i]); })
-        .attr("y", function(d) { return yScale(d); })
-        .attr("fill", function(d,i) {if (i<firstN) return "red"; else return "blue";})
-        .attr("stroke", "white")
-        .attr("stroke-width", 1);
-
-    var yAxis = d3.svg.axis()
-        .orient("left")
-        .ticks(5)
-        .scale(yScale);
-
-    var xAxis = d3.svg.axis()
-        .orient("bottom")
-        .ticks(6)
-        .scale(xScale);
-
-    // remove previous axis for previous restaurant
-
-    d3.select(svgname).selectAll("g").remove();
-
-    d3.select(svgname).append("g")
-        .attr("class", "axises")
-        .attr("transform", "translate(50,0)")
-        .call(yAxis);
-
-    d3.select(svgname).append("g")
-        .attr("class", "axises")  // give it a class so it can be used to select only xaxis labels  below
-        .attr("transform", "translate(0,150)")
-        .call(xAxis);
-
-    // d3.select(svgname).selectAll(".xaxis text")  // select all the text elements for the xaxis
-    //       .attr("transform", function(d) {
-    //           return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
-    //     });
-
-}
-
-d3.json("reviews.json", function(json) {
-
-	rating = json;
-});
 
